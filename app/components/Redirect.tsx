b6019c8eb2d1a20eaf8c9865928901991db8912e
Pratik Wadhai <pratikwadhai001@gmail.com>
2024-09-03 08:27:32 +0530

    added all the files

---
+++ app/components/Redirect.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export function Redirect() {
+  const session = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (session?.data?.user) {
+      router.push("/dashboard");
+    }
+  }, [session.data]);
+}
